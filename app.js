82e318aeb3a2f593bc9dc448a87888b47f5e41d8
Veer-Vamshi01 <93063424+Veer-Vamshi01@users.noreply.github.com>
2023-03-11 16:07:18 +0530

    first

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const express =require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+const request=require("request");
+const app=express();
+
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname +"/signup.html");
+});
+
+
+app.post("/",function(req,res){
+    const fname=req.body.fname;
+    const lname=req.body.lname;
+    const email =req.body.email;
+    const data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:fname,
+                    LNAME:lname
+                }
+            }
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+    const url="https://us18.api.mailchimp.com/3.0/lists/619682bf53";
+    
+    const options={
+        
+        method:"POST",
+       
+        auth:process.env.API_KEY
+       
+    }
+  const request=  https.request(url,options,function(response){
+
+    if(response.statusCode===200){
+        res.sendFile(__dirname+"/success.html");
+    }
+    else{
+        res.sendFile(__dirname+"/failure.html");
+    }
+response.on("data",function(data){
+    console.log(JSON.parse(data));
+})
+    })
+    request.write(jsonData);
+    request.end();
+
+    
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT ||3000,function(){
+    console.log("server started");
+})
